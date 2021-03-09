f134b086e1bf0152281f3f7bc967fe3868bb7e2c
CaioKeidi <caiokeidi@gmail.com>
2021-03-08 22:39:05 -0300

    feat: Categorias and new Routes for Blog

--- src/components/ListaPosts.jsx
+++ src/components/ListaPosts.jsx
@@ -7,13 +7,13 @@ const ListaPost = ( { url } ) => {
 
   useEffect(() => {
       busca(url, setPosts)
-    }, [])
+    }, [url])
 
   return (
     <section className="posts container">
       {posts.map((post) => {
           return(
-        <Link key={post.id + 100} to='#' className={`cartao-post cartao-post--${post.categoria}`}>
+        <Link key={post.id + 100} to={`/posts/${post.id}`} className={`cartao-post cartao-post--${post.categoria}`}>
           <article key={post.id}>
             <h3 className="cartao-post__titulo">{post.title}</h3>
             <p className="cartao-post__meta">{post.metadescription}</p>
